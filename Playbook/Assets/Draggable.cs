c2e8a4740675e67691f7f1af85f1f63200eb9eb3
pauljones13 <pjj313@virginmedia.com>
2018-06-12 20:41:56 +0100

    Draggable UI Components

---
+++ Playbook/Assets/Draggable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Draggable : EventTrigger
+{
+	public override void OnDrag(PointerEventData data)
+	{
+		transform.position += (Vector3)data.delta;
+	}
+		
+
+
+}
---
+++ Playbook/Assets/TestDragPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TestDragPanel : Draggable {
+
+
+
+	// testing
+	public override void OnBeginDrag(PointerEventData data)
+	{
+		var im = GetComponent<Image> ();
+		if (!im) {
+			return;
+		}
+
+		im.color = Color.red;
+	}
+	public override void OnEndDrag(PointerEventData data)
+	{
+		var im = GetComponent<Image> ();
+		if (!im) {
+			return;
+		}
+		im.color = Color.white;
+	}
+
+}
